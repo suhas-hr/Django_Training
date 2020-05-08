3e16eeefe404ca7bf530fd71f7bdd79c71c8c779
= <=>
2020-05-07 21:45:12 -0700

    feat(Django_Training): Part 2
    Configured The database
    
    [ICTRAINING-398]

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'suhas',
+        'PASSWORD': 'Iatbfog4e',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
